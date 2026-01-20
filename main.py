93025913536108291ec7e973c2b76a00c1b8ac1c
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2026-01-20 09:33:29 -0700

    also amoong us

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 #Main run file
+
+#creating stuff that is in fact main stuff
+
+from not_the_main import skibidi
---
+++ not_the_main.py
@@ -0,0 +1,4 @@
+#creating stuff that is in fact not the main
+
+def skibidi():
+    print("I like to move it move it")
